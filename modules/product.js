2df2dc8a2c9ac9a8bc1d2ab3d40ec4548e192e0e
refiloematlapeng <matlapengrp@gmail.com>
2021-08-07 02:54:19 +0200

    Redux

---
+++ modules/product.js
@@ -0,0 +1,10 @@
+class Product {
+    constructor(id, ownerId, title, ImageUrl, description, price) {
+        this.id = id;
+        this.ownerId = ownerId;
+        this.ImageUrl = ImageUrl;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+}
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+
+const ProductsOverviewScreen = props => {
+    return <FlatList />;
+    
+};
+
+export default ProductsOverviewScreen;
